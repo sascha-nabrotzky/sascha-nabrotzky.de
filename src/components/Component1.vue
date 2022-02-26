5c44fd682fc017623a56e04d33aeb43cfc0bb7d7
Sascha Nabrotzky <sascha.nabrotzky@online.de>
2022-02-26 17:09:37 +0100

    First commit

---
+++ src/components/Component1.vue
@@ -0,0 +1,4 @@
+<template>
+    <section><strong>Komponente 1</strong></section>
+    <h2>Hier entsteht eine neue Website von Sascha Nabrotzky</h2>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Component1 from '../components/Component1.vue';
+</script>
+
+<template>
+  <main>
+    <h1>Home</h1>
+    Hier Komponente eintragen
+    <Component1 />
+  </main>
+</template>
