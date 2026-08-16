a6924aa23483d89d705df87cbaded0b60310dd91
sascha-nabrotzky <sascha.nabrotzky@online.de>
2026-08-16 16:26:02 +0200

    Remove unused slide-in-right animation and related configurations
    - Deleted the slide-in-right JavaScript file and its references in TypoScript and TSConfig.
    - Removed associated language entries from localization files.
    - Cleaned up CSS by removing styles related to the slide-in-right animation and shorter width frames.

--- packages/site_package/Resources/Public/JavaScript/slide-in-right.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', (event) => {
-    if (document.querySelector('.frame-slide-in-right')) {
-        const element = document.querySelector('.frame-slide-in-right');
-        setTimeout(element.classList.add('appear'), 600);
-    }
-});
